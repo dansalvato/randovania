c73e7d731d3a971707b908bd47e08d63fb18cb79
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-02-26 15:40:50 +0200

    wip2

--- randovania/game_description/world/dock_node.py
+++ randovania/game_description/world/dock_node.py
@@ -1,8 +1,23 @@
-import dataclasses
+from __future__ import annotations
 
-from randovania.game_description.world.dock import DockType, DockWeakness
-from randovania.game_description.world.node import Node
+import dataclasses
+import typing
+
+from randovania.game_description.requirements import Requirement, RequirementAnd
+from randovania.game_description.world.dock import DockType, DockWeakness, DockLockType
+from randovania.game_description.world.node import Node, NodeContext
 from randovania.game_description.world.node_identifier import NodeIdentifier
+
+
+def _resolve_dock_node(context: NodeContext, node: DockNode) -> typing.Optional[Node]:
+    connection = context.patches.dock_connection.get(
+        context.node_provider.identifier_for_node(node),
+        node.default_connection
+    )
+    if connection is not None:
+        return context.node_provider.node_by_identifier(connection)
+    else:
+        return None
 
 
 @dataclasses.dataclass(frozen=True)
@@ -29,3 +44,37 @@ class DockNode(Node):
 
     def __repr__(self):
         return "DockNode({!r} -> {})".format(self.name, self.default_connection)
+
+    def connections_from(self, context: NodeContext) -> typing.Iterator[tuple[Node, Requirement]]:
+        patches = context.patches
+        provider = context.node_provider
+        self_identifier = provider.identifier_for_node(self)
+
+        target_identifier = patches.dock_connection.get(self_identifier, self.default_connection)
+        if target_identifier is None:
+            # Explicitly connected to nothing.
+            return
+        target_node = provider.node_by_identifier(target_identifier)
+
+        forward_weakness = patches.dock_weakness.get(self_identifier, self.default_dock_weakness)
+        requirement = forward_weakness.requirement
+
+        # TODO: only add requirement if the blast shield has not been destroyed yet
+
+        if isinstance(target_node, DockNode):
+            # TODO: Target node is expected to be a dock. Should this error?
+            back_lock = patches.dock_weakness.get(target_identifier, target_node.default_dock_weakness).lock
+
+            if back_lock is None:
+                pass
+
+            elif back_lock.lock_type == DockLockType.FRONT_BLAST_BACK_BLAST:
+                requirement = RequirementAnd([requirement, back_lock.requirement])
+
+            elif back_lock.lock_type in (DockLockType.FRONT_BLAST_BACK_IMPOSSIBLE,
+                                         DockLockType.FRONT_BLAST_BACK_IF_MATCHING):
+                # FIXME: this should check if we've already openend the back
+                if back_lock != forward_weakness or back_lock.lock_type == DockLockType.FRONT_BLAST_BACK_IMPOSSIBLE:
+                    requirement = Requirement.impossible()
+
+        yield target_node, requirement
--- test/game_description/world/test_world_list.py
+++ test/game_description/world/test_world_list.py
@@ -1,11 +1,12 @@
 from frozendict import frozendict
 
-from randovania.game_description.requirements import ResourceRequirement, RequirementAnd
+from randovania.game_description.requirements import ResourceRequirement, RequirementAnd, Requirement
 from randovania.game_description.resources.resource_type import ResourceType
 from randovania.game_description.resources.simple_resource_info import SimpleResourceInfo
 from randovania.game_description.world.area import Area
-from randovania.game_description.world.dock import DockWeakness, DockLockType, DockType
+from randovania.game_description.world.dock import DockWeakness, DockLockType, DockType, DockLock
 from randovania.game_description.world.dock_node import DockNode
+from randovania.game_description.world.node import NodeContext
 from randovania.game_description.world.node_identifier import NodeIdentifier
 from randovania.game_description.world.world import World
 from randovania.game_description.world.world_list import WorldList
@@ -16,8 +17,11 @@ def test_connections_from_dock_blast_shield(empty_patches):
     req_1 = ResourceRequirement(SimpleResourceInfo("Ev1", "Ev1", ResourceType.EVENT), 1, False)
     req_2 = ResourceRequirement(SimpleResourceInfo("Ev2", "Ev2", ResourceType.EVENT), 1, False)
     dock_type = DockType("Type", "Type", frozendict())
-    weak_1 = DockWeakness("Weak 1", DockLockType.FRONT_ALWAYS_BACK_FREE, frozendict(), req_1)
-    weak_2 = DockWeakness("Weak 2", DockLockType.FRONT_BLAST_BACK_BLAST, frozendict(), req_2)
+    weak_1 = DockWeakness("Weak 1", frozendict(), req_1, lock=None)
+    weak_2 = DockWeakness("Weak 2", frozendict(), Requirement.trivial(), lock=DockLock(
+        lock_type=DockLockType.FRONT_BLAST_BACK_BLAST,
+        requirement=req_2,
+    ))
 
     node_1 = DockNode("Node 1", False, None, "", {}, 0,
                       NodeIdentifier.create("W", "Area 2", "Node 2"), dock_type, weak_1)
@@ -29,10 +33,16 @@ def test_connections_from_dock_blast_shield(empty_patches):
 
     world = World("W", [area_1, area_2], {})
     world_list = WorldList([world])
+    context = NodeContext(
+        patches=empty_patches,
+        current_resources={},
+        database=None,
+        node_provider=world_list,
+    )
 
     # Run
-    result_1 = list(world_list.connections_from(node_1, empty_patches))
-    result_2 = list(world_list.connections_from(node_2, empty_patches))
+    result_1 = list(node_1.connections_from(context))
+    result_2 = list(node_2.connections_from(context))
 
     # Assert
     assert result_1 == [
